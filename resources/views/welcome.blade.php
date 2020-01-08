f49db2ffe58f39ac426762107deb02c1a1169bc9
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-01-08 11:45:22 +0700

    belajar view dan route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Flood Monitoring
                 </div>
 
                 <div class="links">

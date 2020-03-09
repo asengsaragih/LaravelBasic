043efabb5db6ffd5737d14d5664deb081d68463a
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-03-10 03:29:16 +0700

    adding static page dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('android', function () {
+    return view('android');
+});
+
+Route::get('iot', function () {
+    return view('iot');
+});

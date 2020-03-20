a4939f1eece0d8271c1d0390e039b2338e6fafe5
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-03-20 23:40:12 +0700

    update snapshot to array

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -50,9 +50,9 @@ function signOut() {
 }
 
 //-------------FUNCTION ALL-----------------------------------
-function getCategoryFlood(debit, level) {
-    debit.replace(" L/m", "");
-    level.replace(" cm", "");
+function getCategoryFlood(rawDebit, rawLevel) {
+    var debit = parseFloat(rawDebit.replace(" L/m", ""));
+    var level = parseFloat(rawLevel.replace(" cm", ""));
 
     if (debit < 1) {
         if (level < 2) {
@@ -207,48 +207,9 @@ function iot2BiggestFlood() {
 }
 
 // chart function
-function showChartIOT1() {
-    database.ref('Recent/Device1').orderByChild('miliestime').on('value' ,function (snapshot) {
-        if (snapshot.exists()) {
-            snapshot.forEach(function (data) {
-                var val = data.val();
-
-                var rawDebit = val.debit;
-                var rawLevel = val.level;
-
-                var debit = parseFloat(rawDebit.replace(" L/m", ""));
-                var level = parseFloat(rawLevel.replace(" cm", ""));
-
-                if (debit < 1) {
-                    if (level < 2) {
-                        console.log("Aman");
-                    } else if (level <= 3.5) {
-                        console.log("Siaga");
-                    } else {
-                        console.log("Siaga");
-                    }
-                } else if (debit < 2.5) {
-                    if (level < 2) {
-                        console.log("Aman");
-                    } else if (level <= 3.5) {
-                        console.log("Siaga");
-                    } else {
-                        console.log("Bahaya");
-                    }
-                } else {
-                    if (level < 2) {
-                        console.log("Aman");
-                    } else if (level <= 3.5) {
-                        console.log("Siaga");
-                    } else {
-                        console.log("Bahaya");
-                    }
-                }
-
-            });
-        }
-    });
-}
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
 
 function number_format(number, decimals, dec_point, thousands_sep) {
     // *     example: number_format(1234.56, 2, ',', ' ');
@@ -273,6 +234,112 @@ function number_format(number, decimals, dec_point, thousands_sep) {
         s[1] += new Array(prec - s[1].length + 1).join('0');
     }
     return s.join(dec);
+}
+
+function showChartIOT1() {
+    database.ref('Recent/Device1').orderByChild('miliestime').on('value' ,function (snapshot) {
+        var returnArr = [];
+
+        snapshot.forEach(function (data) {
+            var val = data.val();
+            // item.key = data.debit;
+
+            returnArr.push(val.debit);
+        });
+
+        console.log(returnArr);
+    });
+}
+
+function chartCallback(dataLabel, dataRaw) {
+    var ctx = document.getElementById("IOT1Chart");
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+                label: "Earnings",
+                lineTension: 0.3,
+                backgroundColor: "rgba(78, 115, 223, 0.05)",
+                borderColor: "rgba(78, 115, 223, 1)",
+                pointRadius: 3,
+                pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointBorderColor: "rgba(78, 115, 223, 1)",
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                pointHitRadius: 10,
+                pointBorderWidth: 2,
+                data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'date'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 7
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return '$' + number_format(value);
+                        }
+                    },
+                    gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                intersect: false,
+                mode: 'index',
+                caretPadding: 10,
+                callbacks: {
+                    label: function(tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+                    }
+                }
+            }
+        }
+    });
 }
 
 // end chart function
